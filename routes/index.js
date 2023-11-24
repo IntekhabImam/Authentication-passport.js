060089ed18f830ca6786dce25645e74bbef5ffdb
Intekhab Imam <111379442+IntekhabImam@users.noreply.github.com>
2023-11-24 18:46:14 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,51 @@
+var express = require("express");
+var router = express.Router();
+const userModel = require("./users");
+const passport = require("passport");
+const localStrategy = require("passport-local");
+
+passport.use(new localStrategy(userModel.authenticate()));
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/login", function (req, res, next) {
+  res.render("login");
+});
+
+router.get("/profile", isLoggedIn, function (req, res, next) {
+  res.send("profile");
+});
+
+router.post("/register", function (req, res) {
+  const { username, email, fullname } = req.body;
+  const userData = new userModel({ username, fullname, email });
+
+  userModel.register(userData, req.body.password).then(function () {
+    passport.authenticate("local")(req, res, function () {
+      res.redirect("/profile");
+    });
+  });
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/profile",
+    failureRedirect: "/",
+  })
+);
+
+router.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect("/");
+}
+
+module.exports = router;
